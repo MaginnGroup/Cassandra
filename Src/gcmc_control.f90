e4daae28e467114d7d387ac8897a85637ff541d5
Brian <byoo1@nd.edu>
2015-06-07 13:51:54 -0400

    fixed bugs in mie potential

--- Src/gcmc_control.f90
+++ Src/gcmc_control.f90
@@ -135,10 +135,9 @@ SUBROUTINE GCMC_Control
 
   CALL Get_CBMC_Info
 
-  DO i=1, nbr_boxes
-          IF (int_vdw_sum_style(i) == vdw_mie) THEN
-                  CALL Get_Mie_Nonbond
-          END IF
-  END DO
+  IF (int_vdw_sum_style(i) == vdw_mie) THEN
+      CALL Get_Mie_Nonbond
+  END IF
+
   
 END SUBROUTINE GCMC_Control
