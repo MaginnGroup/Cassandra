9ec409f2e457204dc0b25b7dcfc897559b3d2a3d
eliseo <emarinri@nd.edu>
2015-06-24 11:54:57 -0400

    fixed bug regarding getting mie potential info for each box in control.f90 files

--- Src/nptmc_control.f90
+++ Src/nptmc_control.f90
@@ -153,9 +153,10 @@ SUBROUTINE NPTMC_Control
   CALL Get_Dihedral_Atoms_To_Place  
 
   ! Determine whether mie potentials are used
+  DO i=1,nbr_boxes
   IF (int_vdw_sum_style(i) == vdw_mie) THEN
       CALL Get_Mie_Nonbond
   END IF
-
+  END DO
   
 END SUBROUTINE NPTMC_Control
