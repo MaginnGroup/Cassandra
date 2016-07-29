05701e1c37562b86dd5de40f640068ca8e1c987f
Ryan Mullen <rmullen2@nd.edu>
2016-07-29 16:14:55 -0400

    debugging Test8

--- Scripts/testSuite/Test8_Coul_StartEnergy.py
+++ Scripts/testSuite/Test8_Coul_StartEnergy.py
@@ -26,7 +26,7 @@ import re
 elementary_charge = 1.60217662 # 10^-19, C
 epsilon_0 = 8.85418782 # 10^-12, C^2 / N m^2
 avogadro = 6.0221409 # 10^23, /mol
-charge_factor = elementary_charge**2 * avogadro/(4*pi*epsilon_0) * 10000 # kJ A / mol
+charge_factor = elementary_charge**2 * avogadro/(4*np.pi*epsilon_0) * 10000 # kJ A / mol
 
 # Atomic parameters
 charge = [1.0, 1.0]
@@ -34,8 +34,9 @@ charge = [1.0, 1.0]
 # Check parameters
 numChecks = 2
 distList = [1.0,10.]
-analyticList = [] #this list will hold the analytic answers
-cassTestList = [] #this list will hold cassandra's answers
+analyticList = [None] * numChecks #this list will hold the analytic answers
+cassTestList = [None] * numChecks #this list will hold cassandra's answers
+cassStr = "Total system energy" # this string uniquely identifies cassandra's answer in the logfile
 
 # Formatting variables
 bold = '\033[1m' #Will make text bold
@@ -85,16 +86,16 @@ print "\n\n"+bold+"Test 8: Coulombic energy" + normal
 # 1.1) Write MCF with two point charges
 mcf = open(mcfName,"w") #Creates mcf file and allows for edits
 mcf.write("# Atom_Info\n2\n")
-mcf.write("1    A    A   %.3f 1.0   NONE\n\n" % (charge[0]))
-mcf.write("2    A    A   %.3f 1.0   NONE\n\n" % (charge[1]))
-mcf.write("# Bond_Info\n0\n\n")
-mcf.write("# Angle_Info\n0\n\n")
-mcf.write("# Dihedral_Info\n0\n\n")
-mcf.write("# Improper_Info\n0\n\n")
-mcf.write("# Fragment_Info\n0\n\n")
-mcf.write("# Fragment_Connectivity\n0\n\n")
-mcf.write("# Intra_Scaling\n0. 0. 0. 0.\n0. 0. 0. 0.\n\n\n")
-mcf.write("END")
+mcf.write("1    A    A   %.3f 1.0   NONE\n" % (charge[0]))
+mcf.write("2    A    A   %.3f 1.0   NONE\n" % (charge[1]))
+mcf.write("\n# Bond_Info\n0\n")
+mcf.write("\n# Angle_Info\n0\n")
+mcf.write("\n# Dihedral_Info\n0\n")
+mcf.write("\n# Improper_Info\n0\n")
+mcf.write("\n# Fragment_Info\n0\n")
+mcf.write("\n# Fragment_Connectivity\n0\n")
+mcf.write("\n# Intra_Scaling\n0. 0. 0. 0.\n0. 0. 0. 0.\n")
+mcf.write("\nEND\n")
 mcf.close()
 
 # 1.2) Write inp file
@@ -103,7 +104,7 @@ inp.write("# Run_Name\ntest8.out\n\n")
 inp.write("# Sim_Type\nnvt\n\n")
 inp.write("# Nbr_Species\n1\n\n")
 inp.write("# VDW_Style\nnone\n\n")
-inp.write("# Charge_Style\nminimum_image\n\n")
+inp.write("# Charge_Style\ncoul minimum_image\n\n")
 inp.write("# Seed_Info\n1 2\n\n")
 inp.write("# Rcutoff_Low\n0.1\n\n")
 inp.write("# Molecule_Files\n%s 2\n\n" % (mcfName))
@@ -115,7 +116,7 @@ inp.write("# Done_Probability_Info\n\n")
 inp.write("# Start_Type\nread_config 2 %s\n\n" % (xyzName))
 inp.write("# Run_Type\nEquilibration 100\n\n")
 inp.write("# Simulation_Length_Info\nunits steps\nprop_freq 1\ncoord_freq 1\nrun 0\n\n")
-inp.write("END")
+inp.write("END\n")
 inp.close()
 
 # Loop through checks
@@ -125,7 +126,7 @@ for i in range(numChecks):
 	d = distList[i] # atomic separation
 
 	# Step 2) Calculate the correct answer
-  analyticList[i] = charge[0]*charge[1] / d * charge_factor
+	analyticList[i] = charge[0]*charge[1] / d * charge_factor
 
 	# Step 3) Run Cassandra to get its answer
 	# 3.1) Write xyz
@@ -148,12 +149,12 @@ for i in range(numChecks):
 	for line in log:
 		if re.match("(.*)" + cassStr + "(.*)",line):
 			lineStr = line
-	    break
- 
+			break
+
 	# extract the numbers from each line of script extracted from the files above. 
 	for t in lineStr.split():
 		try:
-			cassTestList[1] = float(t)
+			cassTestList[i] = float(t)
 		except ValueError:
 			pass
 
@@ -164,6 +165,6 @@ for i in range(numChecks):
 		print "%-8.1f %12.6e %12.6e %12.6e\n" % (d,cassTestList[i],analyticList[i],errorRel)
 
 if all(passCheck):
-		print bold + "Test 8: Coulombic Energy" + normal
-	else:
-		print bold + "Test Fails" + normal
+	print bold + "Test 8: Coulombic Energy" + normal
+else:
+	print bold + "Test Fails" + normal
