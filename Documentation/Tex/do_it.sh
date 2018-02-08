3831046bf5233cff4c20bdb1103d1655d873c9f5
Eliseo Marin <meliseo@vt.edu>
2018-02-07 23:11:14 -0500

    DOCS: build docs in MacOS

--- Documentation/Tex/do_it.sh
+++ Documentation/Tex/do_it.sh
@@ -4,5 +4,8 @@ bibtex user_guide
 bibtex user_guide
 latex user_guide.tex
 dvips -o user_guide.ps user_guide.dvi
-ps2pdf user_guide.ps
 
+#ps2pdf user_guide.ps
+
+# For MacOS
+dvipdfmx user_guide.dvi
