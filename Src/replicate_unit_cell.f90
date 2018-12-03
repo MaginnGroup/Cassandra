53f7bfdeab55fef6ead72b28cb287fd65cb9418d
ShahResearchGroup <jindal.shah@okstate.edu>
2018-12-03 08:38:03 -0600

    Grid locations computed in fractional units along with other changes

--- Src/replicate_unit_cell.f90
+++ Src/replicate_unit_cell.f90
@@ -24,16 +24,29 @@ SUBROUTINE Replicate_Unit_Cell
   USE Global_Variables
   USE Potential_Map_Variables
   USE IO_Utilities
+  USE File_Names
 
   IMPLICIT NONE
 
   INTEGER :: na_total, nb_total, nc_total, i, j, k, iatom
-  INTEGER :: count, zeo_id, this_box
+  INTEGER :: count, zeo_id, this_box, is, imol
 
   REAL(DP) :: sx_this, sy_this, sz_this, sx, sy, sz
-  REAL(DP) :: rx_this, ry_this, rz_this
+  REAL(DP) :: rx_this, ry_this, rz_this, xlattice, ylattice, zlattice
 
+  ! Store the coordinates of the lattice atoms in a one dimensional array
+  is = 1
+  this_box = 1
+  n_lat_atoms = natoms(is) * nmols_to_read(is,this_box)
+  
+  
+  ALLOCATE(x_lat(n_lat_atoms), y_lat(n_lat_atoms),z_lat(n_lat_atoms))
+  
+  WRITE(logunit,*) 
+  WRITE(logunit,'(A,2X,I5)') 'Number of lattice atoms ', n_lat_atoms
+  WRITE(logunit,*)
 
+  
   ! As the input_routines.f90 already checks that the distance between two parallel faces is at least
   ! twice the cutoff, we need to expand the central simulation cell in both the positive and negative
   ! directions by a unit.
@@ -57,33 +70,33 @@ SUBROUTINE Replicate_Unit_Cell
   ! an actual simulation, we cannot identify the id of the zeolite
   ! (and we don't need to) because the mcf file for the zeolite
   ! is not provided.
-
-  IF (int_sim_type == sim_pot_map) THEN
-
-     DO i = 1, nspecies
-
-        IF (species_list(i)%int_insert == int_noinsert) THEN
-
-           zeo_id = i
-
-           EXIT
-
-        END IF
-
-     END DO
-
-     IF (i == (nspecies+1)) THEN
-
-        ! We did not find a zeolite species
-        ! trigger an error
-
-        err_msg = ''
-        err_msg(1) = 'Species id of zeolite cound not be determined'
-        CALL Clean_Abort(err_msg,'replicate_unit_cell.f90')
-
-     END IF
-
-  END IF
+!!$
+!!$  IF (int_sim_type == sim_pot_map) THEN
+!!$
+!!$     DO i = 1, nspecies
+!!$
+!!$        IF (species_list(i)%int_insert == int_noinsert) THEN
+!!$
+!!$           zeo_id = i
+!!$
+!!$           EXIT
+!!$
+!!$        END IF
+!!$
+!!$     END DO
+!!$
+!!$     IF (i == (nspecies+1)) THEN
+!!$
+!!$        ! We did not find a zeolite species
+!!$        ! trigger an error
+!!$
+!!$        err_msg = ''
+!!$        err_msg(1) = 'Species id of zeolite cound not be determined'
+!!$        CALL Clean_Abort(err_msg,'replicate_unit_cell.f90')
+!!$
+!!$     END IF
+!!$
+!!$  END IF
 
    ALLOCATE(x_super_lat(n_superlat_atoms), Stat = AllocateStatus)
 
@@ -141,54 +154,83 @@ SUBROUTINE Replicate_Unit_Cell
 
   count = 0
 
+  zeo_id = 1
 
-
-  DO iatom = 1, n_lat_atoms
+  OPEN(unit=21,file='replicated.xyz')
+  WRITE(21,*) n_superlat_atoms
+  WRITE(21,*)
+  
+  DO imol = 1, nmols_to_read(zeo_id,this_box)
+     DO iatom = 1, natoms(zeo_id)
      ! obtain the fractional coordinates of this atom
-     CALL Cartesian_To_Fractional(x_lat(iatom),y_lat(iatom),z_lat(iatom),sx,sy,sz,this_box)
-     ! Now generate 27 replicas of this atoms
+        xlattice = atom_list(iatom,imol,this_box)%rxp
+        ylattice = atom_list(iatom,imol,this_box)%ryp
+        zlattice = atom_list(iatom,imol,this_box)%rzp
      
-     DO i = -1,1
+        CALL Cartesian_To_Fractional(xlattice,ylattice,zlattice,sx,sy,sz,this_box)
 
-        DO j = -1, 1
+!!$        IF (sx < 0.0_DP .OR. sx > 1.0_DP) THEN
+!!$           write(*,*) 'sx exceeded'
+!!$           stop
+!!$        END IF
+!!$         IF (sy < 0.0_DP .OR. sy > 1.0_DP) THEN
+!!$           write(*,*) 'sy exceeded'
+!!$           stop
+!!$        END IF
+!!$         IF (sz < 0.0_DP .OR. sz > 1.0_DP) THEN
+!!$            write(*,*) 'sz exceeded'
+!!$            write(*,*) sz
+!!$            write(*,*) xlattice, ylattice, zlattice
+!!$           stop
+!!$        END IF
 
-           DO k = -1, 1
+        ! Now generate 27 replicas of this atoms
+        
+        DO i = -1,1
+           
+           DO j = -1, 1
+              
+              DO k = -1, 1
+                 
+                 count = count + 1
+                 
+                 sx_this = sx + REAL(i,DP)
+                 sy_this = sy + REAL(j,DP)
+                 sz_this = sz + REAL(k,DP)
+                 
+                 ! now obtain the Cartesian coordinates of the super lattice atom
+                 
+                 CALL Fractional_To_Cartesian(sx_this, sy_this, sz_this, &
+                      rx_this, ry_this, rz_this, this_box)
+                 
+                 x_super_lat(count) = rx_this
+                 y_super_lat(count) = ry_this
+                 z_super_lat(count) = rz_this
+                 
+                 ! assign the vDW type and charge to this atom
+                 
+                 IF (int_sim_type == sim_pot_map) THEN
+                    
+                    type_super_lat(count) = nonbond_list(iatom,zeo_id)%atom_type_number
+                    charge_super_lat(count) = nonbond_list(iatom,zeo_id)%charge
+                    
+                 END IF
+                 
+                 WRITE(21,'(A,3x,4(F15.5,3X))') nonbond_list(iatom,zeo_id)%element, &
+                      x_super_lat(count), y_super_lat(count), z_super_lat(count)
+                 
+              END DO ! k = -1, 1
+              
+           END DO ! j = -1, 1
+           
+        END DO ! i = -1, 1
+        
+     END DO ! iatom = 1, natoms(zeo_id)
 
-              count = count + 1
+  END DO ! imol = 1, nmols_to_read(zeo_id,this_box)
 
-              sx_this = sx + REAL(i,DP)
-              sy_this = sy + REAL(j,DP)
-              sz_this = sz + REAL(k,DP)
-
-              ! now obtain the Cartesian coordinates of the super lattice atom
-
-              CALL Fractional_To_Cartesian(sx_this, sy_this, sz_this, &
-                   rx_this, ry_this, rz_this, this_box)
-
-              x_super_lat(count) = rx_this
-              y_super_lat(count) = ry_this
-              z_super_lat(count) = rz_this
-
-              ! assign the vDW type and charge to this atom
-
-              IF (int_sim_type == sim_pot_map) THEN
-
-                 type_super_lat(count) = nonbond_list(iatom,zeo_id)%atom_type_number
-                 charge_super_lat(count) = nonbond_list(iatom,zeo_id)%charge
-
-              END IF
-
-              WRITE(21,'(A,3x,4(F9.5,3X))') nonbond_list(iatom,zeo_id)%element, &
-                   x_super_lat(count), y_super_lat(count), z_super_lat(count)
-
-           END DO ! k = -1, 1
-
-        END DO ! j = -1, 1
-
-     END DO ! i = -1, 1
-    
-  END DO ! iatom = 1, n_lat_atoms
-
+  CLOSE(UNIT=21)
+  
   IF (n_superlat_atoms /= count) THEN
 
      err_msg = ''
@@ -222,6 +264,8 @@ SUBROUTINE Generate_Grid
   IMPLICIT NONE
 
   INTEGER :: i, nxtotal, nytotal, nztotal
+  INTEGER :: ix, iy, iz, n_total_grids, count, ibox
+  REAL(DP) :: x_this, y_this, z_this, sx, sy, sz
 
   ! Determine number of grids in each of the directions
 
@@ -232,6 +276,7 @@ SUBROUTINE Generate_Grid
   ! Recalculate the grid spacing in each of the directions in fractional coordinates
 
   a_spacing = 1.0_DP/REAL(na_grid,DP)
+!  write(*,*) a_spacing, box_list(1)%basis_length, grid_spacing
   b_spacing = 1.0_DP/REAL(nb_grid,DP)
   c_spacing = 1.0_DP/REAL(nc_grid,DP)
 
@@ -250,73 +295,43 @@ SUBROUTINE Generate_Grid
   WRITE(logunit, '(A50,F10.8)') 'Actual grid spacing in b  direction is', b_spacing * box_list(1)%basis_length(2)
   WRITE(logunit,'(A50,F10.8)') 'Actual grid spacing in c direction is', c_spacing * box_list(1)%basis_length(3)
 
-
-  ! allocate memory for grid locations
+  CALL Allocate_Grid_Locations
+ 
   
-  ALLOCATE(a_grid(na_grid), Stat = AllocateStatus)
-
-  IF (AllocateStatus /= 0) THEN
-
-     err_msg = ''
-     err_msg(1) = 'Memory could not be assigned to the grid array in the a direction'
-     err_msg(2) = 'Probably grid size is too small'
-     err_msg(3) = 'Check grid spacing in the input file'
-     CALL Clean_Abort(err_msg,'Generate_Grid')
-
+  n_total_grids = na_grid * nb_grid * nc_grid
+  ALLOCATE(xgrid(n_total_grids), ygrid(n_total_grids), zgrid(n_total_grids), Stat = AllocateStatus)
+  IF (AllocateStatus /=0) THEN
+     err_msg =''
+     err_msg(1) = 'Memoery could not be allocated for storing grid coordinates'
+     CALL Clean_Abort('Generate_Grid', err_msg)
   END IF
 
-  ALLOCATE(b_grid(nb_grid), Stat = AllocateStatus)
+  ! find the grid coordinates
+  count = 0
+  ibox = 1
+  DO ix = 1, na_grid
+     DO iy = 1, nb_grid
+        DO iz = 1,nc_grid
 
-  IF (AllocateStatus /= 0) THEN
+           count = count + 1
+           sx = a_grid(ix)
+           sy = b_grid(iy)
+           sz = c_grid(iz)
 
-     err_msg = ''
-     err_msg(1) = 'Memory could not be assigned to the grid array in the b direction'
-     err_msg(2) = 'Probably grid size is too small'
-     err_msg(3) = 'Check grid spacing in the input file'
-     CALL Clean_Abort(err_msg,'Generate_Grid')
+           CALL Fractional_To_Cartesian(sx,sy,sz,x_this,y_this,z_this,ibox)
+           xgrid(count) = x_this
+           ygrid(count) = y_this
+           zgrid(count) = z_this
 
-  END IF
-
-
-  ALLOCATE(c_grid(nc_grid), Stat = AllocateStatus)
-
-  IF (AllocateStatus /= 0) THEN
-
-     err_msg = ''
-     err_msg(1) = 'Memory could not be assigned to the grid array in the c direction'
-     err_msg(2) = 'Probably grid size is too small'
-     err_msg(3) = 'Check grid spacing in the input file'
-     CALL Clean_Abort(err_msg,'Generate_Grid')
-
-  END IF
-
-
-  ! the grid coordinates start at (0,0,0)
-
-  a_grid(1) = 0.0_DP
-  b_grid(1) = 0.0_DP
-  c_grid(1) = 0.0_DP
-
-  ! x_grid goes from zero to alpha - xstep, the point at alpha is the
-  ! same as the one at zero. Note that the grids are in fractional coordinates
-
-  DO i = 2, na_grid
-
-     a_grid(i) = a_grid(i-1) + a_spacing
-
+        END DO
+     END DO
   END DO
-
-  DO i = 2, nb_grid 
-
-     b_grid(i) = b_grid(i-1) + b_spacing
-
-  END DO
-
-  DO i = 2, nc_grid 
-
-     c_grid(i) = c_grid(i-1) + c_spacing
-
-  END DO
+  
+ ! write(*,*) 'grids'
+!  write(*,*) a_grid(1), a_grid(na_grid), a_spacing
+!  write(*,*) b_grid(1), b_grid(nb_grid), b_spacing
+!  write(*,*) c_grid(1), c_grid(nc_grid), c_spacing
+!  write(*,*) 'total number of grids', na_grid * nb_grid * nc_grid
 
 
   WRITE(logunit,*) 
@@ -395,6 +410,9 @@ SUBROUTINE Generate_Neighbor_List
   LOGICAL :: l_first
 
   Type(Box_Class) :: small_box
+
+  ! Degbugging variables
+  INTEGER :: ii, this_atom
 
   ! Determine number of boxes in each direction
 
@@ -489,7 +507,8 @@ SUBROUTINE Generate_Neighbor_List
 
   ! The padding is half the length of the body diagonal.
   pad_distance = 0.5_DP * SQRT(pad_distance_sq)
-  
+
+!  write(*,*) pad_distance, 'padding'
   ! now compute the cutoff to be used for constructing neighbor list
   cutoff_sq = (rcut_vdw(1)+pad_distance) ** 2
 
@@ -503,6 +522,13 @@ SUBROUTINE Generate_Neighbor_List
   ! By default, energy is to be computed for all the grid points
   link_zeo(:,:,:) = .TRUE.
 
+!!$  write(*,*) 'number of boxes'
+!!$  write(*,*) 'x', xbox
+!!$  write(*,*) 'y', ybox
+!!$  write(*,*) 'z', zbox
+
+  ibox = 1
+  
   DO i = 1, xbox
 
      DO j = 1, ybox
@@ -511,14 +537,14 @@ SUBROUTINE Generate_Neighbor_List
 
            ! Find the center of this box in terms of fractional coordinates
           
-           sx = (REAL(i,DP) - 0.5_DP) * lxbox/box_list(1)%basis_length(1)
-           sy = (REAL(j,DP) - 0.5_DP) * lybox/box_list(1)%basis_length(2)
-           sz = (REAL(k,DP) - 0.5_DP) * lzbox/box_list(1)%basis_length(3)
+           sx = (REAL(i,DP) - 0.5_DP) * lxbox/box_list(1)%basis_length(1) - 0.5_DP
+           sy = (REAL(j,DP) - 0.5_DP) * lybox/box_list(1)%basis_length(2) - 0.5_DP
+           sz = (REAL(k,DP) - 0.5_DP) * lzbox/box_list(1)%basis_length(3) - 0.5_DP
 
            ! obtain the Cartesian coordinates of the center
 
            CALL Fractional_To_Cartesian(sx,sy,sz,xc,yc,zc,ibox)
-
+!           write(23,*)'c', xc, yc, zc
            ! Now loop over all the zeolite atoms (replicated super cell)
 
            l_first = .TRUE.
@@ -534,7 +560,7 @@ SUBROUTINE Generate_Neighbor_List
               IF ( rsq > cutoff_sq ) CYCLE
 
               IF (l_first) THEN
-                 ! this is the first atom for interaction 
+                ! this is the first atom for interaction 
                  link_zeo_start(i,j,k) = n_interaction + 1
                  l_first = .FALSE.
               END IF
@@ -562,13 +588,109 @@ SUBROUTINE Generate_Neighbor_List
               link_zeo_end(i,j,k) = n_interaction
            END IF
 
+           ! check the zeolite atoms around this point
+
+!          write(24,'(5(I8,2x))') i, j, k, link_zeo_end(i,j,k) - link_zeo_start(i,j,k)
+!!$
+!!$           WRITE(24,*) link_zeo_end(i,j,k) - link_zeo_start(i,j,k) + 2
+!!$           WRITE(24,*)
+!!$           WRITE(24,*) 'C', xc, yc, zc
+!!$           DO ii = link_zeo_start(i,j,k), link_zeo_end(i,j,k)
+!!$              ! identify the atom
+!!$              this_atom = zeo_atom_int(ii)
+!!$              WRITE(24,'(A,2X,3(F10.6,2X))') 'H',  x_super_lat(this_atom), &
+!!$                   y_super_lat(this_atom), z_super_lat(this_atom)
+!!$           END DO
+!!$           stop
+
         END DO
 
      END DO
 
   END DO
 
-
+  
+!write(*,*) 'total number of interactions to be evaluated', n_interaction
 
 END SUBROUTINE Generate_Neighbor_List
 
+SUBROUTINE Allocate_Grid_Locations
+
+  USE Global_Variables, ONLY: err_msg, AllocateStatus
+  USE Potential_Map_Variables
+
+  IMPLICIT NONE
+  INTEGER :: i
+  
+  
+ ! allocate memory for grid locations
+  
+  IF (.NOT. ALLOCATED(a_grid))  ALLOCATE(a_grid(na_grid), Stat = AllocateStatus)
+  
+  IF (AllocateStatus /= 0) THEN
+
+     err_msg = ''
+     err_msg(1) = 'Memory could not be assigned to the grid array in the a direction'
+     err_msg(2) = 'Probably grid size is too small'
+     err_msg(3) = 'Check grid spacing in the input file'
+     CALL Clean_Abort(err_msg,'Generate_Grid')
+
+  END IF
+
+  IF (.NOT. ALLOCATED(b_grid))  ALLOCATE(b_grid(nb_grid), Stat = AllocateStatus)
+
+  IF (AllocateStatus /= 0) THEN
+
+     err_msg = ''
+     err_msg(1) = 'Memory could not be assigned to the grid array in the b direction'
+     err_msg(2) = 'Probably grid size is too small'
+     err_msg(3) = 'Check grid spacing in the input file'
+     CALL Clean_Abort(err_msg,'Generate_Grid')
+
+  END IF
+
+
+  IF (.NOT. ALLOCATED(c_grid)) ALLOCATE(c_grid(nc_grid), Stat = AllocateStatus)
+
+  IF (AllocateStatus /= 0) THEN
+
+     err_msg = ''
+     err_msg(1) = 'Memory could not be assigned to the grid array in the c direction'
+     err_msg(2) = 'Probably grid size is too small'
+     err_msg(3) = 'Check grid spacing in the input file'
+     CALL Clean_Abort(err_msg,'Generate_Grid')
+
+  END IF
+
+
+  ! the grid coordinates start at (-0.5,-0.5,-.5) as the original box has been moved
+  ! such that the fractional coordinates in any direction are between -0.5 and 0.5
+
+  a_grid(1) = -0.5_DP
+  b_grid(1) = -0.5_DP
+  c_grid(1) = -0.5_DP
+
+  ! x_grid goes from zero to alpha - xstep, the point at alpha is the
+  ! same as the one at zero. Note that the grids are in fractional coordinates
+
+  DO i = 2, na_grid
+
+     a_grid(i) = a_grid(i-1) + a_spacing
+
+  END DO
+! write(*,*) a_grid
+  DO i = 2, nb_grid 
+
+     b_grid(i) = b_grid(i-1) + b_spacing
+
+  END DO
+
+  
+  DO i = 2, nc_grid 
+
+     c_grid(i) = c_grid(i-1) + c_spacing
+ !    write(*,*) i, c_grid(i)
+
+  END DO
+
+END SUBROUTINE Allocate_Grid_Locations
