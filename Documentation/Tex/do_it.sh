15a65d71e36e3a548d90ebe5017ee6e11702f702
Jacob Gerace <jgerace@nd.edu>
2019-02-19 13:27:15 -0500

    Edited IS docs again

--- Documentation/Tex/do_it.sh
+++ Documentation/Tex/do_it.sh
@@ -5,7 +5,7 @@ bibtex user_guide
 latex user_guide.tex
 dvips -o user_guide.ps user_guide.dvi
 
-#ps2pdf user_guide.ps
+ps2pdf user_guide.ps
 
 # For MacOS
-dvipdfmx user_guide.dvi
+#dvipdfmx user_guide.dvi
