c555120d6e0d49ff128d08aa448e7a6550c31260
Ryan Smith <rsmith56@nd.edu>
2021-03-22 13:33:16 -0400

    Output widom_avg to 16 sigfigs and disallow widom_avg < 1e-99.

--- Src/write_widom_properties.f90
+++ Src/write_widom_properties.f90
@@ -57,9 +57,10 @@ SUBROUTINE Write_Widom_Properties(is,this_box,widom_avg)
   END IF
   !CALL Write_Properties_Buffer(i)
   !widom_avg = widom_sum / species_list(is)%insertions_in_step(this_box)
+  IF (widom_avg < 1.0e-99) widom_avg = 0.0_DP
   IF (is_sweeps) THEN
-          WRITE(this_unit,'(I16,10X,E30.22)') i_mcstep/steps_per_sweep, widom_avg
+          WRITE(this_unit,'(I16,10X,E30.16)') i_mcstep/steps_per_sweep, widom_avg
   ELSE
-          WRITE(this_unit,'(I16,10X,E30.22)') i_mcstep, widom_avg
+          WRITE(this_unit,'(I16,10X,E30.16)') i_mcstep, widom_avg
   END IF
 END SUBROUTINE Write_Widom_Properties
