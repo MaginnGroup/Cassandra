d0bca9bad150c758cd40eab9152a4d039f921097
Ryan Smith <rsmith56@nd.edu>
2024-03-12 15:38:04 -0400

    Allow RB torsions to be written to ring fragment MCF files.

--- Src/participation.f90
+++ Src/participation.f90
@@ -1151,7 +1151,9 @@ CONTAINS
 
        this_dihedral = dihed_id(i)
 
-       IF (dihedral_list(this_dihedral,is)%int_dipot_type == int_opls) THEN
+       SELECT CASE(dihedral_list(this_dihedral,is)%int_dipot_type)
+       !IF (dihedral_list(this_dihedral,is)%int_dipot_type == int_opls) THEN
+       CASE(int_opls)
 
           WRITE(201,'(I5,2X,4(I4,2X),A4,2X,4(F8.3,2X))') i, &
                first_atom(i), second_atom(i), third_atom(i), fourth_atom(i), &
@@ -1161,7 +1163,8 @@ CONTAINS
                dihedral_list(this_dihedral,is)%dihedral_param(3)/kjmol_to_atomic, &
                dihedral_list(this_dihedral,is)%dihedral_param(4)/kjmol_to_atomic
 
-       ELSE IF (dihedral_list(this_dihedral,is)%int_dipot_type == int_charmm) THEN
+       !ELSE IF (dihedral_list(this_dihedral,is)%int_dipot_type == int_charmm) THEN
+       CASE(int_charmm)
           
           WRITE(201,'(I5,2X,4(I4,2X), A6, 2X, F8.3, 2X, F8.0, 2X, F8.3)') i, &
                first_atom(i), second_atom(i), third_atom(i), fourth_atom(i), &
@@ -1170,22 +1173,23 @@ CONTAINS
                dihedral_list(this_dihedral,is)%dihedral_param(2), &
                dihedral_list(this_dihedral,is)%dihedral_param(3) * ( 180.0_DP/PI)
 
-       ELSE IF (dihedral_list(this_dihedral,is)%int_dipot_type == int_amber) THEN
+       !ELSE IF (dihedral_list(this_dihedral,is)%int_dipot_type == int_amber) THEN
 
-          WRITE(201,'(I5,2X,4(I4,2X),A5,2X,9(F8.3,2X))') i, &
-               first_atom(i), second_atom(i), third_atom(i), fourth_atom(i), &
-               'AMBER', &
-               dihedral_list(this_dihedral,is)%dihedral_param(1)/kjmol_to_atomic, &
-               dihedral_list(this_dihedral,is)%dihedral_param(2), &
-               dihedral_list(this_dihedral,is)%dihedral_param(3) * (180_DP/PI), &
-               dihedral_list(this_dihedral,is)%dihedral_param(4)/kjmol_to_atomic, &
-               dihedral_list(this_dihedral,is)%dihedral_param(5), &
-               dihedral_list(this_dihedral,is)%dihedral_param(6) * (180_DP/PI), &
-               dihedral_list(this_dihedral,is)%dihedral_param(7)/kjmol_to_atomic, &
-               dihedral_list(this_dihedral,is)%dihedral_param(8), &
-               dihedral_list(this_dihedral,is)%dihedral_param(9) * (180_DP/PI)
+       !   WRITE(201,'(I5,2X,4(I4,2X),A5,2X,9(F8.3,2X))') i, &
+       !        first_atom(i), second_atom(i), third_atom(i), fourth_atom(i), &
+       !        'AMBER', &
+       !        dihedral_list(this_dihedral,is)%dihedral_param(1)/kjmol_to_atomic, &
+       !        dihedral_list(this_dihedral,is)%dihedral_param(2), &
+       !        dihedral_list(this_dihedral,is)%dihedral_param(3) * (180_DP/PI), &
+       !        dihedral_list(this_dihedral,is)%dihedral_param(4)/kjmol_to_atomic, &
+       !        dihedral_list(this_dihedral,is)%dihedral_param(5), &
+       !        dihedral_list(this_dihedral,is)%dihedral_param(6) * (180_DP/PI), &
+       !        dihedral_list(this_dihedral,is)%dihedral_param(7)/kjmol_to_atomic, &
+       !        dihedral_list(this_dihedral,is)%dihedral_param(8), &
+       !        dihedral_list(this_dihedral,is)%dihedral_param(9) * (180_DP/PI)
                
-       ELSE IF (dihedral_list(this_dihedral,is)%int_dipot_type == int_harmonic) THEN
+       !ELSE IF (dihedral_list(this_dihedral,is)%int_dipot_type == int_harmonic) THEN
+       CASE(int_harmonic)
           
           WRITE(201,'(I5,2X, 4(I4,2X), A8,2X, 2(F8.3,2X))') i, &
                first_atom(i), second_atom(i), third_atom(i), fourth_atom(i), &
@@ -1193,13 +1197,26 @@ CONTAINS
                dihedral_list(this_dihedral,is)%dihedral_param(1)/ kboltz, &
                dihedral_list(this_dihedral,is)%dihedral_param(2) * (180.0_DP/PI) 
 
-       ELSE IF (dihedral_list(this_dihedral,is)%int_dipot_type == int_none) THEN
+       !ELSE IF (dihedral_list(this_dihedral,is)%int_dipot_type == int_none) THEN
+       CASE(int_none)
 
           WRITE(201,'(I5,2X,4(I4,2X),A4)') i, &
                first_atom(i), second_atom(i), third_atom(i), fourth_atom(i), 'none'
+       CASE(int_rb_torsion)
+
+          WRITE(201,'(I5,2X,4(I4,2X),A4,2X,6(F8.3,2X))') i, &
+               first_atom(i), second_atom(i), third_atom(i), fourth_atom(i), &
+               'RB', &
+               dihedral_list(this_dihedral,is)%rb_c(0)/kjmol_to_atomic, &
+               dihedral_list(this_dihedral,is)%rb_c(1)/kjmol_to_atomic, &
+               dihedral_list(this_dihedral,is)%rb_c(2)/kjmol_to_atomic, &
+               dihedral_list(this_dihedral,is)%rb_c(3)/kjmol_to_atomic, &
+               dihedral_list(this_dihedral,is)%rb_c(4)/kjmol_to_atomic, &
+               dihedral_list(this_dihedral,is)%rb_c(5)/kjmol_to_atomic
+       END SELECT
 
           
-       END IF
+       !END IF
 
     END DO
 
